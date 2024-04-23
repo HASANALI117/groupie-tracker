7fd34a792f4fbdd98863e6577ca3d2fb1515b9cf
a7med3365 <a7med3365@gmail.com>
2024-04-23 21:15:24 +0300

    Add panic recovery middleware to HTTP handlers

--- cmd/groupie-tracker/main.go
+++ cmd/groupie-tracker/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"fmt"
 	"groupie-tracker/pkg/handlers"
+	"groupie-tracker/pkg/middleware"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", handlers.MainHandler)
-	http.HandleFunc("/artist/", handlers.ArtistHandler)
+	http.HandleFunc("/", middleware.RecoverPanic(handlers.MainHandler))
+	http.HandleFunc("/artist/", middleware.RecoverPanic(handlers.ArtistHandler))
 	fmt.Println("Server is running on port 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
