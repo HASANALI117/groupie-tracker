f1a1cf18555930d9e9726446a4bd872ab9c206c2
a7med3365 <a7med3365@gmail.com>
2024-04-28 22:47:48 +0300

    adding bad request and internal server error

--- pkg/handlers/errors.go
+++ pkg/handlers/errors.go
@@ -5,11 +5,29 @@ import (
 	"net/http"
 )
 
-func ErrorHandler(w http.ResponseWriter, msg string) {
+func NotFoundError(w http.ResponseWriter, msg string) {
 	ErrorData := models.Error{
 		ErrorCode: 404,
 		ErrorMsg:  msg,
 		ErrorDsc:  "Sorry, we couldn't find the page you're looking for.",
+	}
+	templates.ExecuteTemplate(w, "error.html", ErrorData)
+}
+
+func BadRequestError(w http.ResponseWriter, msg string) {
+	ErrorData := models.Error{
+		ErrorCode: 400,
+		ErrorMsg:  msg,
+		ErrorDsc:  "Sorry, we couldn't process your request.",
+	}
+	templates.ExecuteTemplate(w, "error.html", ErrorData)
+}
+
+func InternalServerError(w http.ResponseWriter, msg string) {
+	ErrorData := models.Error{
+		ErrorCode: 500,
+		ErrorMsg:  msg,
+		ErrorDsc:  "Sorry, we encountered an error while processing your request.",
 	}
 	templates.ExecuteTemplate(w, "error.html", ErrorData)
 }
